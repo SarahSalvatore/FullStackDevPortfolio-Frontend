b05102c3cd68984e3d202bf79e83cb3d27313707
Sarah Salvatore <sarah.h.salvatore@gmail.com>
2022-07-13 16:40:02 -0400

    Add blimp and fix hero button hover.

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { HashLink } from "react-router-hash-link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
+import blimp from "../Assets/Images/blimp.png";
 import htmlCloud from "../Assets/Images/html-cloud.png";
 import cssCloud from "../Assets/Images/css-cloud.png";
 import jsCloud from "../Assets/Images/js-cloud.png";
@@ -16,24 +17,29 @@ const Hero = (props) => {
       className="hero__container"
       style={{ backgroundImage: `url('${props.onToggleChange}')` }}
     >
-      <div className="hero__text">
-        <h3 className="hello-text">Hello, I'm</h3>
-        <h1 className="my-name-text">Sarah Salvatore</h1>
-        <h2 className="my-title-text">Full Stack Developer</h2>
-        <HashLink to="#about">
-          <button className="hero-button">
-            <FontAwesomeIcon icon={faAngleDown} className="button-icon" />
-            LEARN MORE
-          </button>
-        </HashLink>
+      <div className="blimp-div">
+        <img className="blimp" src={blimp} alt="Flying Blimp" />
       </div>
-      <div className="hero-image-container">
-        <img className="html-cloud" src={htmlCloud} alt="HTML5 Cloud" />
-        <img className="css-cloud" src={cssCloud} alt="CSS3 Cloud" />
-        <img className="js-cloud" src={jsCloud} alt="JavaScript Cloud" />
-        <img className="node-cloud" src={nodeCloud} alt="Node.js Cloud" />
-        <img className="react-cloud" src={reactCloud} alt="React Cloud" />
-        <img className="git-cloud" src={gitCloud} alt="Git Cloud" />
+      <div className="text-cloud-container">
+        <div className="hero__text">
+          <h3 className="hello-text">Hello, I'm</h3>
+          <h1 className="my-name-text">Sarah Salvatore</h1>
+          <h2 className="my-title-text">Full Stack Developer</h2>
+          <HashLink to="#about">
+            <button className="hero-button">
+              <FontAwesomeIcon icon={faAngleDown} className="button-icon" />
+              LEARN MORE
+            </button>
+          </HashLink>
+        </div>
+        <div className="hero-image-container">
+          <img className="html-cloud" src={htmlCloud} alt="HTML5 Cloud" />
+          <img className="css-cloud" src={cssCloud} alt="CSS3 Cloud" />
+          <img className="js-cloud" src={jsCloud} alt="JavaScript Cloud" />
+          <img className="node-cloud" src={nodeCloud} alt="Node.js Cloud" />
+          <img className="react-cloud" src={reactCloud} alt="React Cloud" />
+          <img className="git-cloud" src={gitCloud} alt="Git Cloud" />
+        </div>
       </div>
     </section>
   );
